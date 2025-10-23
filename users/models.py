20522bb79f2e568a50cc2cf1e908d4e3648fb02a
R1Jedi <stas.lukin.2014@mail.ru>
2025-10-23 11:00:51 +0300

    пофиксил отправку письма с внедренным celary

--- users/models.py
+++ users/models.py
@@ -1,9 +1,7 @@
-from django.conf import settings
 from django.contrib.auth.models import AbstractUser
-from django.core.mail import send_mail
 from django.db import models
-from django.urls import reverse
 from django.utils.timezone import now
+from .tasks import send_verification_email_async
 
 
 class User(AbstractUser):
@@ -23,19 +21,12 @@ class EmailVerification(models.Model):
     def __str__(self):
         return f"EmailVerification object for {self.user.email}"
 
-    def send_verification_email(self):
-        link = reverse('users:email_verification', kwargs={'email': self.user.email, 'code': self.code})
-        verification_link = f'{settings.DOMAIN_NAME}{link}'
-        subject = f'Потверждение учетной записи для {self.user.username}'
-        message = 'Для потверждения учетной записи для {} перейдите по ссылке: {}'.format(
-            self.user.email,
-            verification_link
-        )
-        send_mail(
-            subject=subject,
-            message=message,
-            from_email=settings.EMAIL_HOST_USER,
-            recipient_list=[self.user.email],
+    def send_verification_email_async(self):
+        send_verification_email_async.delay(
+            user_id=self.user.id,
+            code=str(self.code),
+            user_email=self.user.email,
+            username=self.user.username
         )
 
     def is_expired(self):
