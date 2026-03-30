b60660a7c39e46aaca82885e45fcd751f17496e5
R1Jedi <stas.lukin.2014@mail.ru>
2026-03-30 14:51:00 +0300

    поменял настройку celary (убрал захардкоженный элемент в коде)

--- store/settings.py
+++ store/settings.py
@@ -133,8 +133,8 @@ CACHES = {
 }
 
 # Celery Configuration
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = f'redis://{REDIS_HOST}:{REDIS_PORT}/0'
+CELERY_RESULT_BACKEND = f'redis://{REDIS_HOST}:{REDIS_PORT}/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
